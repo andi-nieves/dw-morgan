ec688829d2fe2493d0e5430d079aa09490ff53e4
andi-nieves <andinieves151720@gmail.com>
2021-06-19 09:19:22 +0800

    Added distinct date & total number of records

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
+var moment = require('moment');
 
 var { queryHandler } = require('./utils');
 var { dbClass } = require('./db');
@@ -14,11 +15,17 @@ app.get('/top/confirmed', async function (req, res) {
     const observation_date = req.query.observation_date;
     const max_results = req.query.max_results;
     var result = {};
+    var dates = [];
+    var total = null;
     try {
         result = await db.query({
             text: `SELECT * FROM ${db.table} ${observation_date ? `WHERE "ObservationDate" = $1` : ''}  ORDER BY "Confirmed" DESC ${max_results ? `LIMIT ${max_results}` : ''}`,
             values: observation_date && [db.parseDate(observation_date)]
         });
+        dates = await db.query(`SELECT DISTINCT "ObservationDate" FROM ${db.table};`);
+        dates = dates.rows.map(d => moment(d.ObservationDate, 'YYYY-MM-DD').format('YYYY-MM-DD'));
+        total = await db.query(`SELECT COUNT(*) FROM ${db.table};`);
+        total = total.rows[0].count
     } catch (error) {
         return res.status(400).json({message: error.message});
     }
@@ -27,7 +34,9 @@ app.get('/top/confirmed', async function (req, res) {
     } else {
         return res.status(200).json({
             observation_date: observation_date,
-            countries: result.rows
+            countries: result.rows,
+            dates,
+            total,
         });
     }
 })
